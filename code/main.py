91ae90160e2cc2d40e0bd7220bbc590d990358fc
oystehbr <oysteinrbk@hotmail.com>
2022-03-07 11:39:14 +0100

    correlation matrix push

--- code/main.py
+++ code/main.py
@@ -6,17 +6,9 @@ import optuna
 import sklearn
 import torch
 import torchvision
-
+import seaborn as sns
 from IPython import embed
 
-
-def read_data():
-    
-    prediction_input = pd.read_parquet("../data/prediction_input.parquet")
-    df2 = pd.read_parquet("../data/input_dataset-2.parquet")
-    embed()
-    
-    
 
 def write_data():
     # TOFAST FOR YOU
@@ -24,16 +16,43 @@ def write_data():
     pass
 
 
+def read_data():
+    
+    prediction_input = pd.read_parquet("../data/prediction_input.parquet")
+    df2 = pd.read_parquet("../data/input_dataset-2.parquet")
+
+    # use the necessary parameters for the prediction
+    df2 = df2.drop(['Bolt_1_Steel tmp',
+        'Bolt_1_Torsion', 
+        'Bolt_2_Torsion',
+        'Bolt_3_Torsion',
+        'Bolt_4_Torsion',
+        'Bolt_5_Torsion',
+        'Bolt_6_Torsion',
+        'lower_bearing_vib_vrt',
+        'turbine_bearing_vib_vrt',], 
+        axis = 1
+        )
+
+    # plotting the correlation matrix, of the data of prediction
+    corr_df2 = df2.corr()
+    fig, ax = plt.subplots(figsize=(18,12))
+    sns.heatmap(corr_df2, annot=True)
+    plt.savefig('../output/correlation_df2.png')
+    # plt.show()
+    plt.close()
+   
+    embed()
+
+    
+    
+    
+
+
+
 
 def main():
-
     read_data()
-    a = 2
-    b = 3
-    abc = a + b
-
-    print(abc)
-    pass
 
 if __name__ == '__main__':
     main()
