b392c96a92fb9f69de7bd66638854759ec625546
sigurho <sigurd.holmsen@gmail.com>
2022-03-07 17:24:49 +0100

    ff

---
+++ sigurd_hack/test.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython import embed
+
+input = pd.read_parquet("data/prediction_input.parquet")
+input = input[input["Turbine_Pressure Drafttube"].isna() == False]
+df = pd.read_parquet("data/input_dataset-2.parquet")
+df = df[df["Bolt_1_Tensile"].isna() == False]
+n_rows = df.shape[0]
+df["dates"] = df.index
+df.index = np.arange(n_rows)
+n_rows_input = input.shape[0]
+input["dates"] = input.index
+input.index = np.arange(n_rows_input)
+
+
+def plot_normalized(inputs, min_time, max_time):
+    for input in inputs:
+        stad = np.std(df[input].iloc[min_time:max_time])
+        plt.plot ((df[input].iloc[min_time:max_time] - np.mean(df[input].iloc[min_time:max_time]))/stad, label=input)
+    plt.legend()
+    plt.show()
+
+min_time = 1000; max_time = 1500
+inputs=["Bolt_1_Tensile", "Turbine_Pressure Drafttube"]
+plot_normalized(inputs, min_time, max_time)
+
