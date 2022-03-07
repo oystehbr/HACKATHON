06eb6056d5ffb55a36b48731174fda0e759d21eb
oystehbr <oysteinrbk@hotmail.com>
2022-03-07 09:24:04 +0100

    setup-of folders

---
+++ data/main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import lightgbm as lgb
+import optuna
+import sklearn
+import torch
+import torchvision
+
+from IPython import embed
+
+
+def read_data():
+    
+    pass
+
+def write_data():
+    # TOFAST FOR YOU
+    # HAAAAAAAALLO
+    pass
+
+
+
+def main():
+    a = 2
+    b = 3
+    abc = a + b
+
+    print(abc)
+    pass
+
+if __name__ == '__main__':
+    main()
+    
+#
